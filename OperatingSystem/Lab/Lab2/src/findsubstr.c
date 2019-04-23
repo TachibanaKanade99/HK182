3420fbd61a3f9ea36efc8863a378fe9e82dd4758
Kanade-Chan <sad361975@gmail.com>
2019-04-23 23:58:43 +0700

    Fix condition to return 1 in findsubstr.c

--- OperatingSystem/Lab/Lab2/src/findsubstr.c
+++ OperatingSystem/Lab/Lab2/src/findsubstr.c
@@ -3,14 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find_sub_string(const char *str, const char *sub){
+void build_PrefixArr(const char *sub, int *prefArr){
     //String length and Substring length:
-    int str_length = strlen(str);
     int sub_length = strlen(sub);
 
     //Find Prefix array:
     int length = 0;
-    int prefArr[sub_length];
     prefArr[0] = 0;
 
     for (int i = 1; i < sub_length; i++){
@@ -21,7 +19,7 @@ int find_sub_string(const char *str, const char *sub){
         else
         {
             if (length != 0){
-                length = prefArr[length -1 ];
+                length = prefArr[length -1];
                 i = i - 1;
             }
             else
@@ -29,9 +27,18 @@ int find_sub_string(const char *str, const char *sub){
                 prefArr[i] = 0;
             }
             
-        }
-        
+        }    
     }
+}
+
+int find_sub_string(const char *str, const char *sub){
+    //Call string length and substring length:
+    int str_length = strlen(str);
+    int sub_length = strlen(sub);
+
+    //Find prefix array for substring:
+    int prefArr[sub_length];
+    build_PrefixArr(sub, prefArr);
 
     //Match substring and string
     int len = 0;
@@ -39,8 +46,8 @@ int find_sub_string(const char *str, const char *sub){
         if (sub[len] == str[i]){
             len = len + 1;
 
-            if (i == sub_length - 1 || i == str_length - 1){
-                //stop when reach the end of string or substring:
+            if (len == sub_length){
+                //stop when reach the end of the substring:
                 return 1;
             }
         }
--- OperatingSystem/Lab/Lab2/src/findsubstr.h
+++ OperatingSystem/Lab/Lab2/src/findsubstr.h
@@ -1,6 +1,7 @@
 #ifndef FIND_SUBSTR_H
 #define FIND_SUBSTR_H
 
+void build_PrefixArr(const char *sub, int *prefArr);
 int find_sub_string(const char *str, const char *sub);
 
 #endif
