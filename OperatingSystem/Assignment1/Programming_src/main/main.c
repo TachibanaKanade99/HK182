f9321f9481eb79d813a4abc22dcadc78d2b98393
TachibanaKanade99 <sad361975@gmail.com>
2019-05-16 10:51:34 +0700

    Modify main.c - Third commit

--- OperatingSystem/Assignment1/Programming_src/main/main.c
+++ OperatingSystem/Assignment1/Programming_src/main/main.c
@@ -11,10 +11,10 @@ int main() {
     
     if (procmem(mypid, &info) == 0) 
     { 
-        printf("Student ID: %lu \n", info.studentID); printf("Code segment: %lx-%lx\n", info.start_code, info.end_code); 
-        printf("Data segment: %lx-%lx\n", info.start_data, info.end_data); 
-        printf("Heap segment: %lx-%lx\n", info.start_heap, info.end_heap); 
-        printf("Start stack: %lx\n", info.start_stack); 
+        printf("Student ID: %lu \n", info->studentID); printf("Code segment: %lx-%lx\n", info->start_code, info->end_code); 
+        printf("Data segment: %lx-%lx\n", info->start_data, info->end_data); 
+        printf("Heap segment: %lx-%lx\n", info->start_heap, info->end_heap); 
+        printf("Start stack: %lx\n", info->start_stack); 
     } 
     
     else 
