a912c4a69622a37c830ae83e100eea96de7a48f2
TachibanaKanade99 <sad361975@gmail.com>
2019-05-16 12:47:13 +0700

    Modify procmem.c - Commit 4

--- OperatingSystem/Assignment1/Programming_src/Wrapper/procmem.c
+++ OperatingSystem/Assignment1/Programming_src/Wrapper/procmem.c
@@ -2,17 +2,6 @@
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 
-struct proc_segs{
-    unsigned long studentID;
-    unsigned long start_code;
-    unsigned long end_code;
-    unsigned long start_data;
-    unsigned long end_data;
-    unsigned long start_heap;
-    unsigned long end_heap;
-    unsigned long start_stack;
-};
-
 long procmem(pid_t pid, struct proc_segs *info){
     //TODO: implement the wrapper here:
     long sysvalue;
