7117fd64f78717be74d13335237925b5f6347c95
TachibanaKanade99 <sad361975@gmail.com>
2019-05-16 11:09:09 +0700

    Modify main.c - Commit 4

--- OperatingSystem/Assignment1/Programming_src/main/main.c
+++ OperatingSystem/Assignment1/Programming_src/main/main.c
@@ -7,14 +7,14 @@
 int main() { 
     pid_t mypid = getpid(); 
     printf("PID: %d\n", mypid); 
-    struct proc_segs *info; 
+    struct proc_segs info; 
     
     if (procmem(mypid, &info) == 0) 
     { 
-        printf("Student ID: %lu \n", info->studentID); printf("Code segment: %lx-%lx\n", info->start_code, info->end_code); 
-        printf("Data segment: %lx-%lx\n", info->start_data, info->end_data); 
-        printf("Heap segment: %lx-%lx\n", info->start_heap, info->end_heap); 
-        printf("Start stack: %lx\n", info->start_stack); 
+        printf("Student ID: %lu \n", info.studentID); printf("Code segment: %lx-%lx\n", info.start_code, info.end_code); 
+        printf("Data segment: %lx-%lx\n", info.start_data, info.end_data); 
+        printf("Heap segment: %lx-%lx\n", info.start_heap, info.end_heap); 
+        printf("Start stack: %lx\n", info.start_stack); 
     } 
     
     else 
