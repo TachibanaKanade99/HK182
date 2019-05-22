a53529811c08e998760ffd41fb2aee7c736ffe82
TachibanaKanade99 <sad361975@gmail.com>
2019-05-22 02:47:30 -0700

    Fix stupid bug in struct

--- OperatingSystem/Assignment1/Programming_src/Wrapper/procmem.c
+++ OperatingSystem/Assignment1/Programming_src/Wrapper/procmem.c
@@ -1,7 +1,6 @@
 #include "procmem.h"
 #include <linux/kernel.h>
 #include <sys/syscall.h>
-#define SIZE 100
 
 long procmem(pid_t pid, struct proc_segs *info){
     //TODO: implement the wrapper here:
